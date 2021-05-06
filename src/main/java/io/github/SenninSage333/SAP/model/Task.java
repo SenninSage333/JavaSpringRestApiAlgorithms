7ff2555b3fc9192d5b78b6e57bb2500d58d8f022
SenninSage333 <thomas430333@gmail.com>
2021-05-06 15:56:53 +0200

    minor update

--- src/main/java/io/github/SenninSage333/SAP/model/Task.java
+++ src/main/java/io/github/SenninSage333/SAP/model/Task.java
@@ -30,10 +30,6 @@ public class Task {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getDescription() {
         return description;
     }
